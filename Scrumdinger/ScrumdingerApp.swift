f47b0f4fb8d02b3890f9b2818b86853994fc50c8
U-ma-s <yuma.prodia4989@gmail.com>
2023-04-15 16:22:39 +0900

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,18 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//  
+//  Created by umas on 2023/04/15
+//  
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
