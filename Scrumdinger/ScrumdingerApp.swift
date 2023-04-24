f6cc44d49d67e2bbcdeadaf87a78398471a1fb21
U-ma-s <yuma.prodia4989@gmail.com>
2023-04-24 19:30:01 +0900

    データの永続化

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -10,11 +10,28 @@ import SwiftUI
 
 @main
 struct ScrumdingerApp: App {
-    @State private var scrums = DailyScrum.sampleData//ここがsource of trurhの大元．
+    //@State private var scrums = DailyScrum.sampleData//ここがsource of trurhの大元．
+    @StateObject private var store = ScrumStore()
     
     var body: some Scene {
         WindowGroup {
-            ScrumsView(scrums: $scrums)//子ビューたちに`@State`プロパティへのbindingを渡していく
+            //ScrumsView(scrums: $scrums)//子ビューたちに`@State`プロパティへのbindingを渡していく
+            ScrumsView(scrums: $store.scrums) {//saveAction()のtrairing closureとしてTaskを使用
+                Task {
+                    do {
+                        try await store.save(scrums: store.scrums)
+                    } catch {
+                        fatalError(error.localizedDescription)
+                    }
+                }
+            }
+                .task {
+                    do {
+                        try await store.load()
+                    } catch {
+                        fatalError(error.localizedDescription)
+                    }
+                }
         }
     }
 }
